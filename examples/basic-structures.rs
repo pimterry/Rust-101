8677601235f5497d091408ec94d799e11e17ba20
pimterry <pimterry@gmail.com>
2014-10-17 02:17:38 +0100

    Add some more examples

---
+++ examples/basic-structures.rs
@@ -0,0 +1,18 @@
+fn main() {
+  const RUN_TO: int = 100;
+  let mut n = 1;
+
+  while n <= RUN_TO {
+    if n % 15 == 0 {
+      println!("fizzbuzz");
+    } else if n % 3 == 0 {
+      println!("fizz");
+    } else if n % 5 == 0 {
+      println!("buzz");
+    } else {
+      println!("{}", n);
+    }
+
+    n += 1;
+  }
+}
